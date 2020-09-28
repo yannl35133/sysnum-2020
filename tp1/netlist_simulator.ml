cacbe5adc5896d7046ca6aa0915f16c97953d2e3
gajaloyan <ga@jaloyan.fr>
2020-09-28 10:47:36 +0200

    initial commit

---
+++ tp1/netlist_simulator.ml
@@ -0,0 +1,26 @@
+let print_only = ref false
+let number_steps = ref (-1)
+
+let simulator program number_steps = failwith "netlist_simulator.simulator: Non implementé"
+
+let compile filename =
+  try
+    let p = Netlist.read_file filename in
+    begin try
+        let p = Scheduler.schedule p in
+        simulator p !number_steps
+      with
+        | Scheduler.Combinational_cycle ->
+            Format.eprintf "The netlist has a combinatory cycle.@.";
+    end;
+  with
+    | Netlist.Parse_error s -> Format.eprintf "An error accurred: %s@." s; exit 2
+
+let main () =
+  Arg.parse
+    ["-n", Arg.Set_int number_steps, "Number of steps to simulate"]
+    compile
+    ""
+;;
+
+main ()
