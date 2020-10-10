a3f9eb9b2401f67d10e7fe1f9e823b968f55df73
Yann Leray <yann.leray@ens.psl.eu>
2020-10-10 13:45:05 +0200

    Repaired graph tests

--- tp1/graph_test.ml
+++ tp1/graph_test.ml
@@ -4,7 +4,7 @@ type graph' = int list array
 
 let g1 = [| [1]; [1]; [1] |]
 let g2 = [| []; [2]; [3]; [4]; [0; 1]; |]
-let g3 = [| [1]; [2]; [3] |]
+let g3 = [| [1]; [2]; [3]; [] |]
 let g4 = [| [3]; [0; 2; 4]; [3]; []; [6]; [1; 4; 7]; [3; 8]; [6; 9]; []; [8] |]
 
 let tests : (graph' * bool) list = [g1, true; g2, true; g3, false; g4, false]
@@ -28,7 +28,7 @@ let check_topo_on g =
 let test i (g, b) =
   let g' = graph_of_graph' g in
 
-  Format.printf "Test %d:\n- has_cycle: %s\n topological_sort: %s\n"
+  Format.printf "Test %d:\n- has_cycle: %s\n- topological_sort: %s\n"
     i
     (if has_cycle g' = b then "OK" else "FAIL")
     (try if check_topo_on g then "OK" else "FAIL"
