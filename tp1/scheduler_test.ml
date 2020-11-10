b7354adbcfe9ff68f7219bbe12f120ca625a8402
Yann Leray <yannl35133@gmail.com>
2020-11-10 21:33:05 +0100

    Restored prebuild_sim test

--- tp1/scheduler_test.ml
+++ tp1/scheduler_test.ml
@@ -6,13 +6,24 @@ let filename = ref ""
 let compile filename =
   let ic = Netlist.read_file filename in
   let lexbuf = Lexing.from_channel ic in
-  let oc = open_out ((Filename.chop_suffix filename ".net") ^ "_scheduled.net") in
+  let out_file = (Filename.chop_suffix filename ".net") ^ "_scheduled.net" in
+  let oc = open_out out_file in
   try
     let program = Netlist_parser.program Netlist_lexer.token lexbuf in
     let scheduled_program = Scheduler.schedule program in
 
     Netlist.print_program oc scheduled_program;
-    close_out oc    
+    close_out oc;
+    
+    if not !print_only then
+      let cmd = Format.sprintf "./prebuilt_netlist_simulator.byte %s %s"
+        (
+          if !number_steps = -1 then ""
+          else Format.sprintf "-n %d" !number_steps
+        )
+        out_file
+      in
+      ignore (Unix.system cmd)
   with
     | Netlist_parser.Error ->
         Format.eprintf "@[<v>%a@ Syntax error@]"
